8e79f3c8a5c875ae953d9ce5c3f8f7b22f0199ce
Noah <noahleviwilder@gmail.com>
2018-02-18 19:38:10 -0500

    Add new item functionality complete.

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
 
-    let itemArray = ["Buy Eggs", "Buy Milk", "Buy Flour"]
+    var itemArray = ["Buy Eggs", "Buy Milk", "Buy Flour"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -35,7 +35,7 @@ class TodoListViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
     //    print(itemArray[indexPath.row])
         
-            
+        
         
         
         if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
@@ -47,7 +47,44 @@ class TodoListViewController: UITableViewController {
         
         tableView.deselectRow(at: indexPath, animated: true) // Deselects after being tapped
         
+    }
+    
+    
+    //Mark - Add New Items
+    
+    @IBAction func addButtonPressed(_ sender: Any) {
         
+        var textField = UITextField() // We use this textField as a local variable to store the inputted data from alertTextField
+        
+        let alert = UIAlertController(title: "Add new Todoey item", message: "", preferredStyle: .alert)
+        
+        
+       
+        let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+            // What will happen once the user clicks the Add Item button on our UIAlert
+            
+            if let text : String = textField.text {
+                print(text)
+                self.itemArray.append(text)
+                self.tableView.reloadData()
+            }
+            
+        }
+        
+        
+        alert.addTextField { (alertTextField) in // You chose the name of the text field, in this case I chose alertTextField
+            alertTextField.placeholder = "Create new item"
+            textField = alertTextField
+            // This allows alertTextField to be used at a wider scope and lets texField actually hold the inputted values
+            
+            
+        }
+        
+        
+        alert.addAction(action)
+        
+        
+        present(alert, animated: true, completion: nil)
         
     }
     
